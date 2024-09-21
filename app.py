c850a06476735692e0b2a8b55e378c9d7c9adbc6
Poent <Poent@users.noreply.github.com>
2024-09-21 03:03:24 -0600

    updated todo list.

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import json
 from bnet_auth_api import BnetAuthApi
 from bnet_ah_api import BnetAhApi, AuctionDatabase
 
+
+# TODO: Implement proper functions for calculating item stats and table stats
+# TODO: fix auction_database.py so that it tracks duplicate snapshot data somehow, 
+#       since the timestamp changes based on the pull time, and not the time the 
+#       snapshot was taken...
+# TODO: Implement Current Price function to accurately calcualte the current price of an item. Will use "market value" calculation (bottom 15% of prices). 
+# TODO: Implement equation for calculating xmute profit based on tracked items and current prices
+
+
 def load_xmute_commodities(file_path='xmute_commodities.json'):
     """Load the xmute_commodities.json file."""
     with open(file_path, 'r') as file:
