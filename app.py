22b3eca6665526e5381699d428fe6b20e8c0d640
Poent <poent.z@gmail.com>
2024-09-26 21:05:24 -0600

    cleanup

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import json
 from bnet_auth_api import BnetAuthApi
 from bnet_ah_api import BnetAhApi, AuctionDatabase
 
-from utils import load_xmute_commodities, get_item_name_by_id
+from utils import load_xmute_commodities, get_item_name_by_id, group_and_print_data, print_table_stats
+
+DEBUG = False
 
 # Main logic
 if __name__ == "__main__":
@@ -12,7 +14,6 @@ if __name__ == "__main__":
     # Load client credentials from auth.json
     with open('auth.json') as file:
         auth_data = json.load(file)
-
 
     # Initialize the database handler
     db_handler = AuctionDatabase(db_path='commodities.db')
@@ -29,76 +30,15 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"Error fetching commodities data: {e}")
 
-    # Store tracked items using the loaded xmute data
+
+    # Store "tracked items" using the loaded xmute data
     db_handler.store_tracked_items(xmute_data)
-
-    # Get table stats for the commodities table (last AH snapshot)
-    print("Stats for the commodities table:")
-    table_stats = db_handler.get_table_stats("commodities")
-    
-    if table_stats:
-        total_auctions = table_stats.get("total_auctions")
-        unique_items = table_stats.get("unique_item_ids")
-        oldest_auction = table_stats.get("oldest_active_auction")
-        
-        print(f"    Total auctions: {total_auctions}, Unique items: {unique_items}, Last Snapshot Timestamp: {oldest_auction}")
-    else:
-        print("    No data available")
-
-
-    #get stats for the tracked_items table
-    print("Stats for the tracked_items table:")
-    table_stats = db_handler.get_table_stats("tracked_items")
-
-    if table_stats:
-        total_auctions = table_stats.get("total_auctions")
-        unique_items = table_stats.get("unique_item_ids")
-        oldest_auction = table_stats.get("oldest_active_auction")
-
-        print(f"    Total auctions: {total_auctions}, Unique items: {unique_items}, Oldest Auction: {oldest_auction}")
-    else:
-        print("    No data available")
-
-
-
-    # for each item in xmute_commodities.json, get the stats for the tracked_items table
-
-    # Initialize a dictionary to group results by tier
-    grouped_results = {}
-    
-    # Iterate through the data and group by tier
-    for item in xmute_data:
-        for tier in item["tiers"]:
-            item_id = tier["item_id"]
-            item_stats = db_handler.get_item_stats(item_id, "tracked_items")
-            if item_stats:
-                active_auctions, min_unit_price = item_stats
-                item_name = get_item_name_by_id(item_id, xmute_data)
-                tier_level = tier['tier']
-                if tier_level not in grouped_results:
-                    grouped_results[tier_level] = []
-                grouped_results[tier_level].append({
-                    "item_name": item_name,
-                    "active_auctions": active_auctions,
-                    "min_unit_price": min_unit_price
-                })
-    
-    # Print the grouped results
-    for tier, items in grouped_results.items():
-        print(f"Tier: {tier}")
-        for item in items:
-            if item["min_unit_price"] is not None:
-                print(f"  {item['item_name']}, Active auctions: {item['active_auctions']}, Minimum unit price: {item['min_unit_price'] / 10000:.2f} g")
-            else:
-                print(f"  {item['item_name']}, Active auctions: {item['active_auctions']}, Minimum unit price: N/A")
-
-    # update the tracked items summary table
     db_handler.update_tracked_items_summary()
-    print("Tracked items summary table updated.")
 
-    #print the tracked items summary table
-    print("Tracked items summary table:")
-    db_handler.update_tracked_items_summary()
+    # some debug output
+    if DEBUG:
+        print_table_stats(db_handler, "tracked_items")
+        group_and_print_data(xmute_data, db_handler)
 
     db_handler.print_tracked_items_summary()
 
--- utils.py
+++ utils.py
@@ -14,3 +14,46 @@ def get_item_name_by_id(item_id, items):
             if tier["item_id"] == item_id:
                 return item["item_name"]
     return "Item ID not found"
+
+
+def group_and_print_data(xmute_data, db_handler):
+    grouped_results = {}
+    
+    for item in xmute_data:
+        for tier in item["tiers"]:
+            item_id = tier["item_id"]
+            item_stats = db_handler.get_item_stats(item_id, "tracked_items")
+            if item_stats:
+                active_auctions, min_unit_price = item_stats
+                item_name = get_item_name_by_id(item_id, xmute_data)
+                tier_level = tier['tier']
+                if tier_level not in grouped_results:
+                    grouped_results[tier_level] = []
+                grouped_results[tier_level].append({
+                    "item_name": item_name,
+                    "active_auctions": active_auctions,
+                    "min_unit_price": min_unit_price
+                })
+    
+    for tier, items in grouped_results.items():
+        print(f"Tier: {tier}")
+        for item in items:
+            if item["min_unit_price"] is not None:
+                print(f"  {item['item_name']}, Active auctions: {item['active_auctions']}, Minimum unit price: {item['min_unit_price'] / 10000:.2f} g")
+            else:
+                print(f"  {item['item_name']}, Active auctions: {item['active_auctions']}, Minimum unit price: N/A")
+
+def print_table_stats(db_handler, table_name):
+    print(f"Stats for the {table_name} table:")
+    table_stats = db_handler.get_table_stats(table_name)
+    
+    if table_stats:
+        total_auctions = table_stats.get("total_auctions")
+        unique_items = table_stats.get("unique_item_ids")
+        oldest_auction = table_stats.get("oldest_active_auction")
+        
+        print(f"    Total auctions: {total_auctions}, Unique items: {unique_items}, Oldest Auction: {oldest_auction}")
+    else:
+        print("    No data available")
+
+
